215eee1f3800eed8239e734ce7f50585a9dfa223
Don Henton <donhenton@gmail.com>
2019-09-19 10:14:07 -0500

    refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,63 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
- 
+
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 var tsify = require('tsify');
+var fancy_log = require('fancy-log');
 var del = require('del');
 var paths = {
-    pages: ['public_html/*.html']
+  pages: ['public_html/*.html']
 };
 
- 
+
 /**
  * Simple transpile for demonstration purposes
  */
 gulp.task('simple-transpile', function () {
   return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest('dist'));
+    .pipe(tsProject())
+    .js.pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
   return del(['dist/**']);
 });
 
 
 gulp.task('copy-html', function () {
   return gulp.src(paths.pages)
-        .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', gulp.series(gulp.parallel('copy-html'), function () {
-  return browserify({
+
+function createBrowserify(watch) {
+
+  var bObj = browserify({
       basedir: '.',
       debug: true,
       entries: ['src/main.ts'],
       cache: {},
       packageCache: {}
-  })
-  .plugin(tsify)
-  .bundle()
-  .pipe(source('bundle.js'))
-  .pipe(gulp.dest('dist'));
+    })
+    .plugin(tsify)
+
+  if (watch) {
+    bObj = watchify(bObj);
+  }
+
+  return bObj;
+
+}
+
+
+gulp.task('build', gulp.series(gulp.parallel('copy-html'), function () {
+  return createBrowserify()
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('dist'));
 }));
+
+ 
