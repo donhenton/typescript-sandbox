2f1f927d0dc4fb373967841e8058bb22b17099c5
Don Henton <donhenton@gmail.com>
2019-11-27 08:50:41 -0600

    adding server

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,13 @@ var source = require('vinyl-source-stream');
 var watchify = require('watchify');
 var tsify = require('tsify');
 var fancy_log = require('fancy-log');
+var browserSync = require('browser-sync').create();
 var terser = require('gulp-terser');
 var del = require('del');
 var paths = {
+  js: ['dist/**/*.js'],
+  ts: ['src/**/*.ts'],
+  css: [],
   pages: ['public_html/*.html']
 };
 
@@ -21,25 +25,25 @@ var paths = {
  */
 gulp.task('simple-transpile', function () {
   return tsProject.src()
-    .pipe(tsProject())
-    .js.pipe(gulp.dest('dist'));
+        .pipe(tsProject())
+        .js.pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function () {
-  return del(['dist/**','release/**']);
+  return del(['dist/**', 'release/**']);
 });
 
 
 gulp.task('copy-html-dev', function () {
   return gulp.src(paths.pages)
-    .pipe(gulp.dest('dist')) ;
+        .pipe(gulp.dest('dist'));
 });
- 
+
 
 gulp.task('copy-html-release', function () {
   return gulp.src(paths.pages)
-   
-    .pipe(gulp.dest('release'));
+
+        .pipe(gulp.dest('release'));
 });
 
 function mainAppBundle(useDebug) {
@@ -52,29 +56,29 @@ function mainAppBundle(useDebug) {
   }
 
   var mainAppBundler = browserify({
-      basedir: '.',
-      debug: debug,
-      entries: ['src/main.ts'],
-      extensions: ['.js'],
-      cache: {},
-      packageCache: {}
-    }).plugin(tsify)
-    .transform("babelify", {
-      presets: ["@babel/preset-env"]
-    });
+    basedir: '.',
+    debug: debug,
+    entries: ['src/main.ts'],
+    extensions: ['.js'],
+    cache: {},
+    packageCache: {}
+  }).plugin(tsify)
+        .transform("babelify", {
+          presets: ["@babel/preset-env"]
+        });
 
 
   return mainAppBundler
-    .bundle()
-    .on('error', fancy_log);
+        .bundle()
+        .on('error', fancy_log);
 
 }
 
 gulp.task('dev-build', gulp.series(gulp.parallel('copy-html-dev'), function () {
   return mainAppBundle(true)
 
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('dist'));
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dist'));
 
 
 }));
@@ -82,25 +86,45 @@ gulp.task('dev-build', gulp.series(gulp.parallel('copy-html-dev'), function () {
 gulp.task('release-setup', gulp.series(gulp.parallel('copy-html-release'), function () {
   return mainAppBundle(false)
 
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('release'));
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('release'));
 
 
 }));
 
+gulp.task('serve', function () {
+
+  browserSync.init({
+    server: "./dist",
+    port: 6060,
+    ui: {port: 6061}
+  });
+
+  gulp.watch(paths.ts).on('change', function(done) {
+     return mainAppBundle(true)
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dist'));
+     done();
+     
+  });
+  gulp.watch(paths.js).on('change', browserSync.reload);
+  gulp.watch(paths.css).on('change', browserSync.reload);
+  gulp.watch(paths.pages).on('change', browserSync.reload);
+});
+
 gulp.task('release-build', gulp.series(
-  'release-setup', 'copy-html-release',
-  function (done) {
+      'release-setup', 'copy-html-release',
+      function (done) {
 
 
-    var t = gulp.src('./release/bundle.js')
-      .pipe(terser())
-       
-      .pipe(gulp.dest('./release'))
+        var t = gulp.src('./release/bundle.js')
+              .pipe(terser())
 
-    done();
-    return t;
+              .pipe(gulp.dest('./release'))
 
-  }
+        done();
+        return t;
+
+      }
 
 ));
--- src/greet.ts
+++ src/greet.ts
@@ -1,3 +1,3 @@
 export function sayHello(name: string) {
-    return `Hello jojo from ${name}`;
+    return `Hello bonzo from ${name}`;
 }
